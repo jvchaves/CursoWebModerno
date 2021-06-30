0b57799b4e64da45421866ef1f8544305b89c402
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-06-30 20:37:57 -0300

    Comecando a trabalhar fundamento

---
+++ Fundamentos/parNomeValor.js
@@ -0,0 +1,21 @@
+const saudacao = 'Ola' 
+
+function exec(){
+    const saudacao = 'Eai'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Joao',
+    idade: 21,
+    peso: 95,
+    endereco:{
+        logradouro: 'Rua Francisco Pinheiro Filho',
+        numero: 382
+
+    }
+
+}
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
