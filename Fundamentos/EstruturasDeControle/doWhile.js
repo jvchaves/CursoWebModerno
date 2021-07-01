7cc11a82b81f8685c3f4a25d53534c4f67a73c9a
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-07-01 20:59:48 -0300

    Utilizando o while e Do while

---
+++ Fundamentos/EstruturasDeControle/doWhile.js
@@ -0,0 +1,11 @@
+function getInteiroAleatorioEntre(min,max){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1
+do {
+    opcao = getInteiroAleatorioEntre(-1,10)
+    console.log("O valor escolhido foi: " + opcao)
+}while(opcao != -1)
+console.log('Até a Proxima!')
